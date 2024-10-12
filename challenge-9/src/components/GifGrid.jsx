7f55035991949c466d9ff6ce80177d7b73d871f8
PostboxRetinal <78338192+PostboxRetinal@users.noreply.github.com>
2024-10-12 08:39:46 -0500

    updated challenge 9. I'm literally borked

--- challenge-9/src/components/GifGrid.jsx
+++ challenge-9/src/components/GifGrid.jsx
@@ -1,17 +1,16 @@
-import { useDeferredValue } from "react";
-import { useEffect } from "react";
-import { propTypes } from 'prop-types';
+import { useEffect, useState } from "react";
+import PropTypes from 'prop-types';
 
 const getGifs = async (category) => {
     const url = `https://api.giphy.com/v1/gifs/search?api_key=9EE68wmo2x5vQQmfjbHe46sJUJ1bp4Qd&q=${category}&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`;
     const res = await fetch(url);
     const { data } = await res.json();
 
-    const gifs = data.map(({ img }) => {
+    const gifs = data.map(({ id, title, images }) => {
         return {
-            id: img.id,
-            title: img.title,
-            url: img.bitly_url
+            id: id,
+            title: title,
+            url: images.original.url
         }
     });
 
@@ -19,18 +18,64 @@ const getGifs = async (category) => {
 }
 
 export const GifGrid = ({ category }) => {
-    getGifs(category);
+    const [inputValue, setInputValue] = useState('');
+    const [gifs, setGifs] = useState([]);
+    const [newCategory, setNewCategory] = useState('');
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => {
+        if (inputValue) {
+            getGifs(inputValue)
+                .then(setGifs);
+        }
+    }, [inputValue]);
+
+    const handleInputChange = (e) => {
+        setInputValue(e.target.value);
+    };
+
+    const handleCategoryChange = (e) => {
+        setNewCategory(e.target.value);
+    };
+
+    const handleAddCategory = () => {
+        setCategories([...categories, newCategory]);
+        setNewCategory('');
+    };
 
     return (
-        <>
+        <div>
             <h3>{category}</h3>
-            <p>Hello World</p>
-        </>
+            <input 
+                type="text" 
+                value={inputValue} 
+                onChange={handleInputChange} 
+                placeholder="Search for gifs" 
+            />
+            <div>
+                {gifs.map(gif => (
+                    <div key={gif.id}>
+                        <h4>{gif.title}</h4>
+                        <img src={gif.url} alt={gif.title} />
+                    </div>
+                ))}
+            </div>
+            <input 
+                type="text" 
+                value={newCategory} 
+                onChange={handleCategoryChange} 
+                placeholder="Add new category" 
+            />
+            <button onClick={handleAddCategory}>Add Category</button>
+            <ul>
+                {categories.map((cat, index) => (
+                    <li key={index}>{cat}</li>
+                ))}
+            </ul>
+        </div>
     )
 }
 
 GifGrid.propTypes = {
-    category: propTypes.string.isRequired
+    category: PropTypes.string.isRequired
 }
-
-export default GifGrid;
